e7f2d19a17e9639e55d8f66082e9b78a66aea8a6
Eddie Lin <eddielin0926@gmail.com>
2024-06-12 17:51:44 +0800

    feat: :fire: setup swagger ui with openapi hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,21 @@
-import { Hono } from 'hono'
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { logger } from "hono/logger";
 
-const app = new Hono()
+const app = new OpenAPIHono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger());
 
-export default app
+// Use the middleware to serve Swagger UI at /ui
+app.get("/ui", swaggerUI({ url: "/doc" }));
+
+// The OpenAPI documentation will be available at /doc
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Biblioklept API",
+  },
+});
+
+export default app;
